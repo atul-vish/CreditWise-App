7ef09286ba0c89cf1affb9e9ad6ea3c26f598713
Atul Vishwakarma <atulvish39@gmail.com>
2026-02-05 17:52:14 +0530

    Main app file

--- CreditWise.py
+++ CreditWise.py
@@ -22,7 +22,7 @@ st.write("Robust & production-safe ML loan approval app")
 # --------------------------------------------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("D:\AIML\ML Projects\clean_dataset.csv")
+    return pd.read_csv("clean_dataset.csv")
 
 
 df = load_data()
